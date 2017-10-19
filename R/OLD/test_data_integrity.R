7cd2053fe980d6168d08e246b0b25ec60c0e2ddf
jog2030@qatar-med.cornell.edu <jog2030@qatar-med.cornell.edu>
2017-10-19 11:10:43 +0000

    Reworking tests a);

--- R/OLD/test_data_integrity.R
+++ R/OLD/test_data_integrity.R
@@ -20,146 +20,95 @@ test_data_integrity <- function(
       utils::installed.packages())
 
 # Processing --------------------------------------------------------------
-  library(package_name, character.only = TRUE)
+  require(package_name, character.only = TRUE)
   testthat::context("--> Checking integrity of data referenced in 'data_catalogue' <--")
 
-  require(package_name, character.only = TRUE)
-  for(entry in names(data_catalogue)){
+  for (entry in data_catalogue)
+  {
+    with(entry,
+         {
+## Test integrity of stored object ----
+           testthat::test_that(
+             paste0("The R opbject based on '",Base.Name,"' matches the checksum."),
+             {
+               testthat::expect_identical(
+                 digest::digest(
+                   object = get0(Base.Name),
+                   algo   = Hashing.Algo,
+                   file   = FALSE),
+                 Hash.Object)
+             })
+## Deal with entries where 'extdata' expected ----
+           if (Data.Type %in% c("file", "url"))
+           {
+             ext_data_path <- system.file(
+               file.path(
+                 "extdata",
+                 Base.Name) %>%
+                 paste0(".zip"),
+               package = package_name,
+               mustWork = FALSE)
+             if (identical(ext_data_path, ""))
+             {
+               warning(
+                 "Catalogue entry '",
+                 entry,
+                 "' possibly separately distributed and not present. Conisder fetching with 'retreive_missing_remote_data'.")
+             } else {
+### Test integrity of compressed 'extdata' ----
+               # zip includes timestamps no matter what settings - files zipped at
+               # different times will never have identical hashes
+               testthat::test_that(
+                 paste0("'", Base.Name, "' in its compressed form matches the checksum."),
+                 {
+                   testthat::expect_identical(
+                     digest::digest(
+                       object = ext_data_path,
+                       algo   = Hashing.Algo,
+                       file   = TRUE),
+                     Hash.Compressed)
+                 })
 
-    source_is_remote <- data_catalogue %>%
-      magrittr::extract2(entry) %>%
-      magrittr::extract2("File.Is.Remote")
+### Test integrity of extracted 'extdata' ----
+               unzip_target_dir <- tempdir()
+               unzipped_file_path <- utils::unzip(
+                 zipfile   = ext_data_path,
+                 exdir     = unzip_target_dir,
+                 junkpaths = TRUE)
+               testthat::test_that(
+                 paste0("'", Base.Name, "' in its decompressed form matches the checksum."),
+                 {
+                   testthat::expect_identical(
+                     digest::digest(
+                       object = unzipped_file_path,
+                       algo   = Hashing.Algo,
+                       file   = TRUE),
+                     Hash.Uncompressed)
+                 })
 
-    # Test integrity of compressed 'extdata'
-    ## Make path
-    file_base_name <- data_catalogue %>%
-      magrittr::extract2(entry) %>%
-      magrittr::extract2("File")
-    file_path_compressed <- system.file(
-      file.path(
-        "extdata",
-        file_base_name) %>%
-        paste0(".zip"),
-      package = package_name,
-      mustWork = source_is_remote %>%
-        magrittr::not())
+### Test equivalency of the stored object/data and the raw data ----
+               # Load dependencies necessary to recreate data structures
+               Package.Dependencies %>%
+                 sapply(
+                   require,
+                   character.only = TRUE)
 
-    ## Test
-    if(file_path_compressed %>%
-       magrittr::equals(""))
-    {
-      warning(
-        "Catalogue entry '",
-        entry,
-        "' separately distributed and not present. Conisder fetching with 'retreive_missing_remote_data'."
-      )
-    } else {
-      # zip includes timestamps no matter what settings - files zipped at
-      # different times will never have identical hashes
-      if(source_is_remote %>%
-         magrittr::not())
-      {
-        testthat::test_that(
-          paste0(
-            "'",
-            file_base_name,
-            "' in its compressed form matches the checksum."),
-          {
-            testthat::expect_identical(
-              digest::digest(
-                file = file_path_compressed,
-                algo = data_catalogue %>%
-                  magrittr::extract2(entry) %>%
-                  magrittr::extract2("Hashing.Algo")),
-              data_catalogue %>%
-                magrittr::extract2(entry) %>%
-                magrittr::extract2("Hash.Compressed"))
-          })
-      }
-    }
+               # Re-parse the raw data content
+               object_fresh <- parse_data(
+                 path             = unzipped_file_path,
+                 reading_function = Parsing.Function,
+                 reading_options  = Parsing.Options)
 
-    # Test integrity of extracted 'extdata'
-    ## Extract the data temporarily
-    if(file_path_compressed %>%
-       magrittr::equals("") %>%
-       magrittr::not())
-    {
-      unzip_target_dir <- tempdir()
-      utils::unzip(
-        zipfile = file_path_compressed,
-        exdir = unzip_target_dir,
-        junkpaths = TRUE)
-      ## Make path
-      file_path <- unzip_target_dir %>%
-        file.path(file_base_name)
-      ## Test
-      testthat::test_that(
-        paste0(
-          "'",
-          file_base_name,
-          "' in its decompressed form matches the checksum."),
-        {
-          testthat::expect_identical(
-            digest::digest(
-              file = file_path,
-              algo = data_catalogue %>%
-                magrittr::extract2(entry) %>%
-                magrittr::extract2("Hashing.Algo")),
-            data_catalogue %>%
-              magrittr::extract2(entry) %>%
-              magrittr::extract2("Hash.Uncompressed"))
-        })
-    }
-
-    # Test integrity of stored object
-    testthat::test_that(
-      paste0(
-        " the R opbject based on '",
-        file_base_name,
-        "' matches the checksum."),
-      {
-        testthat::expect_identical(
-          digest::digest(
-            file_base_name %>%
-              get0(),
-            algo = data_catalogue %>%
-              magrittr::extract2(entry) %>%
-              magrittr::extract2("Hashing.Algo")),
-          data_catalogue %>%
-            magrittr::extract2(entry) %>%
-            magrittr::extract2("Hash.Object"))
-      })
-
-    # Test equivalency of the stored object/data and the raw data
-    ## Load dependencies necessary to recreate data structures
-    data_catalogue %>%
-      magrittr::extract2(entry) %>%
-      magrittr::extract2("File.Reading.Package.Dependencies") %>%
-      sapply(
-        require,
-        character.only = TRUE)
-
-    ## Re-parse the raw data content
-    file_content_fresh <- data_catalogue %>%
-      magrittr::extract2(entry) %>%
-      magrittr::extract2("File.Reading.Function") %>%
-      do.call(
-        c(
-          file_path %>%
-            as.list(),
-          data_catalogue %>%
-            magrittr::extract2(entry) %>%
-            magrittr::extract2("File.Reading.Options")))
-
-    ## Test
-    testthat::test_that(
-      paste0(
-        "The R object derived from '",
-        file_base_name,
-        "' represents the original file."), {
-          testthat::expect_equal(
-            file_base_name %>%
-              get0(),
-            file_content_fresh)})
+               # Test
+               testthat::test_that(
+                 paste0(" the R object derived from '", Base.Name,"' represents the original file."),
+                 {
+                   testthat::expect_equal(
+                     get0(Base.Name),
+                     object_fresh)
+                 })
+             }
+           }
+         })
   }
 }
