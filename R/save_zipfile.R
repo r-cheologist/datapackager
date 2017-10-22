e0ed5056b6a281523a1efc6dede464baf140851c
jog2030@qatar-med.cornell.edu <jog2030@qatar-med.cornell.edu>
2017-10-22 19:00:19 +0000

    Include 'path.expand' to deal e.g. with '~';

--- R/save_zipfile.R
+++ R/save_zipfile.R
@@ -1,10 +1,13 @@
 #' @noRd
 save_zipfile <- function(uncomp_path,target_dir){
+  uncomp_path %<>%
+    path.expand()
   target_path <- target_dir %>%
     file.path(
       uncomp_path %>%
         basename() %>%
-        paste0(".zip"))
+        paste0(".zip")) %>%
+    path.expand()
   utils::zip(
     zipfile = target_path,
     files = uncomp_path,
