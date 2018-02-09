405c7b9192770fed4554cd484049f5d0454c4df2
jog2030@qatar-med.cornell.edu <jog2030@qatar-med.cornell.edu>
2018-02-09 10:43:41 +0100

    Remove a misbehaving (!?) zip CLI option ('R');

--- R/save_zipfile.R
+++ R/save_zipfile.R
@@ -10,8 +10,8 @@ save_zipfile <- function(uncomp_path,target_dir){
     path.expand()
   utils::zip(
     zipfile = target_path,
-    files = uncomp_path,
-    flags = "-j9DXR")
+    files   = uncomp_path,
+    flags   = "-j9DX")
   target_path %>%
     invisible()
 }
